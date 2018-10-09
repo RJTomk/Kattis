00ebb32f0e7ccaeb5fc415802e7e988490d25990
Bestshot99 <cdnaircadet@gmail.com>
2018-10-09 01:17:16 -0300

    Solved Cetvrta and Hissing Microphone

---
+++ Cetvrta/Cetvrta.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Cetvrta{
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		int[] x = new int[3], y = new int[3];
+		for(int i = 0; i < 3; i++){
+			x[i] = reader.nextInt();
+			y[i] = reader.nextInt();
+		}
+		reader.close();
+
+		System.out.printf("%d %d", returnUnused(x), returnUnused(y));
+	}
+
+	private static int returnUnused(int[] arr){
+		return arr[0] == arr[1] ? arr[2] : arr[1] == arr[2] ? arr[0] : arr[1];
+	}
+}
---
+++ Hissing Microphone/Mic.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Mic{
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		String word = reader.next();
+		reader.close();
+
+		for(int i = 0; i < word.length() - 1; i++)
+			if(word.charAt(i) == word.charAt(i + 1) && word.charAt(i) == 's'){
+				System.out.println("hiss");
+				return;
+			}
+
+		System.out.println("no hiss");
+	}
+}
